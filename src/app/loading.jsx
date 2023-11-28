2a9462532d942a3b0fcaf00f833d66c4c6291ed8
StarWindTW <shngjnlan@gmail.com>
2023-11-29 01:37:43 +0800

    update

---
+++ src/app/loading.jsx
@@ -0,0 +1,17 @@
+import Navbar from '@/components/navbar/Navbar'
+import React from 'react'
+const LoadingCircle = () => {
+  return <div className='w-10 h-10 border-2 border-transparent border-r-[#00A9FE] rounded-full border-b-[#00A9FE] animate-spin'></div>
+}
+const Loading = () => {
+  return (
+    <div className='space-y-4'>
+      <Navbar />
+      <div className="fixed inset-0 flex items-center justify-center">
+          <LoadingCircle />
+      </div>
+    </div>
+  )
+}
+
+export default Loading
